2a0b71af3219b35168388f365d8be960dd47691c
metachoSamuel <samuel.f.met@gmail.com>
2022-05-22 01:26:45 -0500

    Aun sufro

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,20 @@
+""" Admin site for Users """
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+#models
+from django.contrib.auth.models import User
+from users.models import User
+
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    """Profile admin"""
+    
+    readonly_fields = ('created', 'modified')
+    
+
+admin.site.register(User, UserAdmin)
+
+    
--- app/users/models.py
+++ app/users/models.py
@@ -1,21 +1,24 @@
 """  User models  """
-
-from django.contrib.auth.models import User
 from django.db import models
+from django import forms
 
 
 # Create your models here.
 
-class Profile(models.Model):
+class User(models.Model):
     """ Profile Model """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    #user_name = models.CharField(max_length=255, blank=False, null=False)
+    #user = models.In(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=255, blank= False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    user_name = models.CharField(max_length=255, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    password = forms.CharField(widget=forms.PasswordInput())
     
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         """ return username """
-        return self.user.username
+        return self.user_name
     
