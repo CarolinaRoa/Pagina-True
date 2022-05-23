264e9a9db6b791d7a7ccc274d60d3e7cb8bdbb09
metachoSamuel <samuel.f.met@gmail.com>
2022-05-23 18:37:34 -0500

    Politicas de tratamiento de datos

--- app/users/views.py
+++ app/users/views.py
@@ -2,34 +2,39 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
 from .models import UserProfile
+from django.contrib import messages
 
 # Create your views here.
 """ Registro de usuarios """
 def user_registration(request):
+    
     data = {
         'form': CustomUserCreationForm()
     }
     print("hola")
     
     if request.method == 'POST':
-        print("estoy dentro de post")
-        form = CustomUserCreationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            user = authenticate(
-                username = form.cleaned_data["username"],
-                password = form.cleaned_data["password"]
-            )
-            login(request, user)
-            print("Sisas")
-            
-            
-            """Redirect to home"""
-            return redirect(to="/")
-            
-            
-        data["form"] = form
+        display_type = request.POST.get("display_type", None)
+        if display_type in ["locationbox", "displaybox"]:
+            print("estoy dentro de post")
+            form = CustomUserCreationForm(data=request.POST)
         
-        
+            if form.is_valid():
+                form.save()
+                user = authenticate(
+                    user_name = form.cleaned_data["user_name"],
+                    password = form.cleaned_data["password"]
+                )
+                login(request, user)
+                #messages.warning(request, 'Your account expires in three days.')
+            
+            
+                """Redirect to home"""
+                return redirect(to="/")
+            
+            
+            data["form"] = form
+        else:
+            messages.warning(request, 'Your account expires in three days.')
         
     return render(request, 'registration/register.html', data)
