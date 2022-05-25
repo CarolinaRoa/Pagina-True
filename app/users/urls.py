dd68a30496a1b79b5968d63465efe6274f994078
metachoSamuel <samuel.f.met@gmail.com>
2022-05-25 00:17:06 -0500

    Arreglando problemas con registro usuarios

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import user_registration
+from .views import user_registration, user_detail
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/', user_registration, name='register'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'),name='login'),
+    path('login/', LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
+    path('user/', user_detail, name='user'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,35 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
 from .models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 """ Registro de usuarios """
 def user_registration(request):
-    data = {
-        'form': CustomUserCreationForm()
-    }
-    print("hola")
     
     if request.method == 'POST':
-        print("estoy dentro de post")
-        form = CustomUserCreationForm(data=request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            user = authenticate(
-                user_name = form.cleaned_data["user_name"],
-                password = form.cleaned_data["password"]
-            )
-            #login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            print("Sisas")
+            username = form.cleaned_data['username']
+            messages.success(request, f'Usuario {username} creado')
+            return redirect(to='/')
+    else:
+        form = UserRegisterForm()
             
-            
-            """Redirect to home"""
-            return redirect(to="/")
-            
-            
-        data["form"] = form
-        
-        
-        
-    return render(request, 'registration/register.html', data)
+    context = { 'form' : form }
+    
+    return render(request, 'registration/register.html', context)
+
+def user_detail(request):
+    return render(request, 'users/user_detail.html')
